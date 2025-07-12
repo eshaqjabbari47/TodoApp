cf69aea158fbc4981a1b9da6c17fcdbec34c77d5
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-07-12 07:12:26 +0430

    add task logic

--- TodoApp/Controllers/HomeController.cs
+++ TodoApp/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TodoApp.Models;
 
 namespace TodoApp.Controllers
 {
     public class HomeController : Controller
     {
+        TodoContext db = new TodoContext();
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Todo todo) {
+            db.Todos.Add(todo);
+            db.SaveChanges();
             return View();
         }
     }
